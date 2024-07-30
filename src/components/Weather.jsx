a9a54e5f155cf9c24ee0c19fafe90f084e3cd672
suresh37 <sureshkarthick37@gmail.com>
2024-07-30 13:53:37 +0530

    Lat Long finder added

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -1,7 +1,51 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 const Weather = () => {
-	return <div>Weather App</div>;
+	const [weatherData, setWeatherData] = useState({ temp: '35' });
+	const [lat, setLat] = useState(0);
+	const [long, setLong] = useState(0);
+	let latitude = '',
+		longitude = '';
+	useEffect(() => {
+		if (navigator.geolocation) {
+			navigator.geolocation.getCurrentPosition(function (position) {
+				setLat(position.coords.latitude.toFixed(6));
+				setLong(position.coords.longitude.toFixed(6));
+				console.log(`Latitude: ${latitude}, Longitude: ${longitude}`);
+			});
+		} else {
+			console.log('Geolocation is not supported');
+		}
+	}, []);
+
+	const handleInputChange = () => {
+		console.log('Input changed');
+	};
+	return (
+		<div>
+			<p style={{ textAlign: 'center' }}>
+				<b>Weathering With U</b>
+			</p>
+			<p>Location : {weatherData?.temp}</p>
+			<p>Current Weather : {weatherData?.temp}</p>
+			<br />
+			<div>
+				<b>Latitude: </b>
+				<input
+					value={lat}
+					onChange={handleInputChange}
+				></input>
+				<br />
+				<br />
+				<b>Longitude: </b>
+				<input
+					value={long}
+					onChange={handleInputChange}
+				></input>
+				<br />
+			</div>
+		</div>
+	);
 };
 
 export default Weather;
